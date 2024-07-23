d46e04c51f7394574160a5c4dec25923cef24d74
akbxr <hi@akbxr.com>
2024-07-22 17:41:29 -0700

    2-2 feat(point): add authentication middleware to redeem points endpoint (#11)
    * refactor: add global declare
    
    * feat(point): add authentication middleware to redeem points endpoint

--- apps/api/src/routers/point.router.ts
+++ apps/api/src/routers/point.router.ts
@@ -1,6 +1,6 @@
 import { PointController } from '@/controllers/point.controller';
+import { authenticate } from '@/middlewares/auth.middleware';
 import { Router } from 'express';
-
 export class PointRouter {
   private router: Router;
   private pointController: PointController;
@@ -12,7 +12,7 @@ export class PointRouter {
   }
 
   private initializeRoutes() {
-    this.router.post('/point/redeem', this.pointController.redeemPointsForTicket);
+    this.router.post('/point/redeem', authenticate, this.pointController.redeemPointsForTicket);
   }
 
   getRouter(): Router {
