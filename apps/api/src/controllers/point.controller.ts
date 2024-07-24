978486ccd5d47b6614bbe0c2af2a32ef82bb9195
akbxr <hi@akbxr.com>
2024-07-24 07:18:51 +0700

    chore: update import statement in point.controller.ts

--- apps/api/src/controllers/point.controller.ts
+++ apps/api/src/controllers/point.controller.ts
@@ -1,6 +1,6 @@
 import { PrismaClient, User, UserRole } from '@prisma/client';
 import { Request, Response } from 'express';
-import prisma from '@/prisma';
+import prisma from '../prisma';
 
 declare global {
   namespace Express {
